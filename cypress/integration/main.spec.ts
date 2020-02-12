c512167e379a40dd481e08b1f4779c0713fc0064
PBM <pbmalecki@wp.pl>
2020-02-12 21:37:26 +0100

    Added main page search input tests

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -10,4 +10,28 @@ describe('Main Page', () => {
     it('has main section heading', () => {
         cy.get('h1').should('contain', 'Post list');
     });
+
+    it('has post tiles', () => {
+        getPostTiles().should('have.length', 4);
+    });
+
+    it('should filter posts based on search input value', () => {
+        getSearchInput().type('First');
+        getPostTiles()
+            .should('have.length', 1)
+            .should('contain.text', 'First post');
+    });
+
+    it('should reset posts on clearing search input', () => {
+        getSearchInput().clear();
+        getPostTiles().should('have.length', 4);
+    });
+
+    function getPostTiles() {
+        return cy.get('.ap-post-tile');
+    }
+
+    function getSearchInput() {
+        return cy.get('.ap-search-input > input');
+    }
 });
