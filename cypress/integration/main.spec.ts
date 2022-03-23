f71b5f73c9410a303ecf164b92d0ab7b11b2ab56
pbmchc <pbmalecki@wp.pl>
2022-03-23 21:22:37 +0100

    test: fix formatting in e2e tests

--- cypress/integration/main.spec.ts
+++ cypress/integration/main.spec.ts
@@ -1,37 +1,32 @@
 describe('Main Page', () => {
-    before(() => {
-        cy.visit('/');
-    });
+  const getPostTiles = () => cy.get('.ap-post-tile');
+  const getSearchInput = () => cy.get('.ap-search-input > input');
 
-    it('has header title', () => {
-        cy.get('.ap-header-title').contains('ANGULAB');
-    });
+  before(() => {
+    cy.visit('/');
+  });
 
-    it('has main section heading', () => {
-        cy.get('h1').should('contain', 'Post list');
-    });
+  it('has header title', () => {
+    cy.get('.ap-header-title').contains('ANGULAB');
+  });
 
-    it('has post tiles', () => {
-        getPostTiles().should('have.length', 4);
-    });
+  it('has main section heading', () => {
+    cy.get('h1').should('contain', 'Post list');
+  });
 
-    it('should filter posts based on search input value', () => {
-        getSearchInput().type('First');
-        getPostTiles()
-            .should('have.length', 1)
-            .should('contain.text', 'First post');
-    });
+  it('has post tiles', () => {
+    getPostTiles().should('have.length', 4);
+  });
 
-    it('should reset posts on clearing search input', () => {
-        getSearchInput().clear();
-        getPostTiles().should('have.length', 4);
-    });
+  it('should filter posts based on search input value', () => {
+    getSearchInput().type('First');
+    getPostTiles()
+      .should('have.length', 1)
+      .should('contain.text', 'First post');
+  });
 
-    function getPostTiles() {
-        return cy.get('.ap-post-tile');
-    }
-
-    function getSearchInput() {
-        return cy.get('.ap-search-input > input');
-    }
+  it('should reset posts on clearing search input', () => {
+    getSearchInput().clear();
+    getPostTiles().should('have.length', 4);
+  });
 });
