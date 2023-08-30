5f923b61066746c9390a1ff8f751b71444a2a211
pbmchc <pbmalecki@wp.pl>
2023-08-30 18:43:31 +0200

    chore: update to cypress 13

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:4200',
         screenshotOnRunFailure: false,
-        supportFile: false,
-        video: false
+        supportFile: false
     }
 });
