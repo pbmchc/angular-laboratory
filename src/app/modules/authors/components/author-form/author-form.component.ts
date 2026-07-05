b9e6ee1bbd64fd22637148f575c5a39462266b41
pbmchc <pbmalecki@wp.pl>
2026-07-05 20:35:04 +0200

    feat: migrate author form to signals

--- src/app/modules/authors/components/author-form/author-form.component.ts
+++ src/app/modules/authors/components/author-form/author-form.component.ts
@@ -1,36 +1,73 @@
-import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Output,
+  signal,
+  Signal
+} from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import {
+  debounce,
+  form,
+  FormField,
+  FormRoot,
+  required,
+  validateAsync
+} from '@angular/forms/signals';
 
-import { AuthorFormValue } from '../../../../shared/models/author/author-form-value.model';
-import { AuthorForm } from '../../../../shared/models/author/author-form.model';
 import { Author } from '../../../../shared/models/author/author.model';
-
-import { AuthorFormBuilder } from './author-form-builder.service';
-import { AUTHOR_FORM_CONTROLS } from './author-form.constants';
+import { getFullName } from '../../../../shared/utils/string.utils';
+import { UniqueAuthorValidator } from '../../shared/validators/unique-author.validator';
 
 @Component({
   selector: 'ap-author-form',
   templateUrl: './author-form.component.html',
   styleUrls: ['./author-form.component.scss'],
-  providers: [AuthorFormBuilder],
-  imports: [FormsModule, ReactiveFormsModule]
+  imports: [FormField, FormRoot]
 })
-export class AuthorFormComponent implements OnInit {
-  private authorFormBuilder = inject(AuthorFormBuilder);
+export class AuthorFormComponent {
+  private uniqueAuthorValidator = inject(UniqueAuthorValidator);
+
+  authorFormModel = signal({ firstName: '', lastName: '' });
+  authorForm = form(
+    this.authorFormModel,
+    (form) => {
+      debounce(form, 'blur');
+      required(form.firstName);
+      required(form.lastName);
+      validateAsync(form, {
+        params: ({ value }) => {
+          const { firstName, lastName } = value();
+          return getFullName(firstName, lastName);
+        },
+        factory: (fullName: Signal<string | undefined>) =>
+          rxResource({
+            params: () => fullName(),
+            stream: ({ params }) => this.uniqueAuthorValidator.validate(params)
+          }),
+        onSuccess: (result) =>
+          result.isUnique
+            ? null
+            : {
+                kind: 'uniqueness',
+                message: $localize`:@@author_form.error.author_exists:Author already exists`
+              },
+        onError: () => ({
+          kind: 'unknown',
+          message: $localize`:@@author_form.error.unknown:Unknown error`
+        })
+      });
+    },
+    {
+      submission: {
+        action: async (fields) => {
+          const { firstName, lastName } = fields().value();
+          this.saveAuthor.emit({ name: getFullName(firstName, lastName) });
+        }
+      }
+    }
+  );
 
   @Output() saveAuthor = new EventEmitter<Pick<Author, 'name'>>();
-  authorForm!: AuthorForm;
-  readonly AUTHOR_FORM_CONTROLS = AUTHOR_FORM_CONTROLS;
-
-  ngOnInit() {
-    this.authorForm = this.authorFormBuilder.buildAuthorForm();
-  }
-
-  onSubmit() {
-    const author = this.authorFormBuilder.formatAuthorFormValue(
-      this.authorForm.value as AuthorFormValue
-    );
-
-    this.saveAuthor.emit(author);
-  }
 }
--- src/app/modules/authors/components/author-form/author-form.constants.ts
+++
@@ -1,4 +0,0 @@
-export const AUTHOR_FORM_CONTROLS = {
-  FIRSTNAME: 'firstName',
-  LASTNAME: 'lastName'
-} as const;
